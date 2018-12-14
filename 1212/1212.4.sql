c46b572adf498763536351df471867a03f409e24
s18003 <s18003@std.it-college.ac.jp>
2018-12-14 00:31:38 +0000

    hire_date削除

--- 1212/1212.4.sql
+++ 1212/1212.4.sql
@@ -1,3 +1,3 @@
 select last_name, hire_date, salary from employees
-where hire_date, salary between '06-01-01' and '06-12-31'
+where salary between '06-01-01' and '06-12-31'
 and (salary > 9000 or commission_pct is null);
