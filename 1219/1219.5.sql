858eeeca0db49001d1a7dcb865dc7ec5e926fe1f
s18003 <s18003@std.it-college.ac.jp>
2019-01-17 07:44:13 +0000

    12/19の課題

--- 1219/1219.5.sql
+++ 1219/1219.5.sql
@@ -1,4 +1,4 @@
 ALTER SESSION SET NLS_DATE_LANGUAGE='AMERICAN';
 select last_name,hire_date, to_char(next_day(ADD_MONTHS(hire_date,6),'月'),'"Monday,the "fmddspth" of "month", "yyyy') "REVIEW" 
-from (select last_name,hire_date,EMPLOYEE_ID from employees order by EMPLOYEE_ID ) 
+from (select last_name,hire_date,employee_id from employees order by employee_id ) 
 where rownum <= 5;
